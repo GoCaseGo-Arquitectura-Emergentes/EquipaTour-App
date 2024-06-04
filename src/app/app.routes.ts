5e12fb533608fa9445d99c95604dd8f5e27ad95a
Jherico <jhericof.sv.189@gmail.com>
2024-06-03 23:40:37 -0500

    login and signup pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { SignupComponent } from './modules/auth/signup/signup.component';
+import { HomeComponent } from './modules/home/home/home.component';
+import { TrainingComponent } from './modules/training/training/training.component';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'training', component: TrainingComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
+];
