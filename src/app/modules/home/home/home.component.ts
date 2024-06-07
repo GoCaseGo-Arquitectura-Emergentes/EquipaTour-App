c0ad731cd45a199a9e9a825fce18efa1b54cd9d5
Jherico <jhericof.sv.189@gmail.com>
2024-06-07 15:55:36 -0500

    home view

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  showLuggageOptions = false;
 
+  constructor() {}
+
+  toggleLuggageOptions() {
+    this.showLuggageOptions = !this.showLuggageOptions;
+  }
 }
