787f22aef37ac064361b44f387c6613517f68b5b
Djalma Dioses <54141599+djalmadioses@users.noreply.github.com>
2024-06-25 15:53:25 -0500

    feat: add crud of tour packages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { SignupComponent } from './modules/auth/signup/signup.component';
 import { HomeComponent } from './modules/home/home/home.component';
 import { TrainingsComponent } from './modules/trainings/trainings/trainings.component';
 import { authGuard } from './core/guards/auth.guard';
+import { TourPackagesComponent } from './tour-experience/pages/tour-packages/tour-packages.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'trainings', component: TrainingsComponent, canActivate: [authGuard] },
+  { path: 'tour-packages', component: TourPackagesComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' },
 ];
---
+++ src/app/tour-experience/models/tour-package.models.ts
@@ -0,0 +1,6 @@
+export class TourPackage {
+    id: number = 0;
+    packageName: string = "";
+    availableSlots: number = 0;
+    guideName: string = "";
+}
